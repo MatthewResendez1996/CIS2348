bda00038ac6be0ad169e2772a5088c14986a5d16
MatthewResendez1996 <70235237+MatthewResendez1996@users.noreply.github.com>
2020-12-01 18:48:53 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+def selection_sort_descend_trace(number):
+    for i in range(len(number)-1):
+        ind = i
+        for j in range(i + 1, len(number)):
+            if number[ind] < number[j]:
+                ind = j
+        number[i], number[ind] = number[ind], number[i]
+        for x in number:
+            print(x,end=" ")
+        print()
+    return number
+
+if __name__ == "__main__":
+    numbers = [int(x) for x in input("").split()]
+    selection_sort_descend_trace(numbers)
