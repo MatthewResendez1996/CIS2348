c85d425d4a135c30fb24d3564d67b779657a8499
MatthewResendez1996 <70235237+MatthewResendez1996@users.noreply.github.com>
2020-12-01 20:15:54 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+#Matthew Resendez
+#1431242
+parts = input().split()
+name = parts[0]
+while name != '-1':
+    try:
+        age = int(parts[1]) + 1
+    except ValueError:
+        age = 0
+    print(name, age)
+    parts = input().split()
+    name = parts[0]
